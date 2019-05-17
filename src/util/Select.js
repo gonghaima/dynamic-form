07c1bc1b206827290ab11d543138867780969220
Steven <gonghaima@hotmail.com>
2019-05-17 16:07:35 +1000

    added options value

--- src/util/Select.js
+++ src/util/Select.js
@@ -1,8 +1,8 @@
 import React from "react";
-export default ({ field, setVal }) => (
+export default ({ field: { options }, setVal }) => (
   <select>
-    <option>1</option>
-    <option>2</option>
-    <option>3</option>
+    {options.map(opt => {
+      return <option>{opt.value}</option>;
+    })}
   </select>
 );
