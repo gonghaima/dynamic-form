0217a8263f86f661a1656e782ce968dc112e658f
Steven <gonghaima@hotmail.com>
2019-05-20 12:32:13 +1000

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import DynamicForm from "./components/Form";
 import "./App.css";
 
@@ -7,10 +6,6 @@ function App() {
   return (
     <div className="App">
       <div className="App-main">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         Learn React
         <div>Main section</div>
         <DynamicForm />
